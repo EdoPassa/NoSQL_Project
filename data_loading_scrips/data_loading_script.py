c15b3a2ea69a1dfc0179031ca8ddf1851ba564ed
Edoardo Passarino <edoardo.passarino@gmail.com>
2023-01-17 16:06:58 +0100

    update

--- data_loading_scrips/data_loading_script.py
+++ data_loading_scrips/data_loading_script.py
@@ -6,28 +6,33 @@ import json
 
 
 def load_data_from_csv(path, filename):
-    df = pd.read_csv(path + '/' + filename)
+    """ Load data from csv file and return a pandas dataframe """
+    df = pd.read_csv(path + '/' + filename)  # read csv file
     return df
 
 
 def load_df_to_mongo(df, mongo_uri, mongo_db):
+    """ Load a pandas dataframe to a mongo database """
     client = MongoClient(mongo_uri)
     db = client[mongo_db]
-    for index, row in df.iterrows():
-        id = row['dialogueID']
-        if db.dialogs.count_documents({"dialogueID": id}, limit=1) != 0:
+    for index, row in df.iterrows():  # iterate over rows in dataframe
+        row_id = row['dialogueID']
+
+        # check if dialogeID already exists and if so it means it's the same conversation just upadates the document
+        if db.dialogs.count_documents({"dialogueID": row_id}, limit=1) != 0:
+
             domanda = row['to']
-            if pd.isnull(domanda):
+            if pd.isnull(domanda):  # check if the row is a question
                 type_sent = 'Domanda'
             else:
                 type_sent = 'Risposta'
-            db.dialogs.update_one({'dialogueID': id}, {'$addToSet': {'chat': [{"sentence": row['text'],
-                                                                               "type": type_sent}]}})
+            db.dialogs.update_one({'dialogueID': row_id}, {'$addToSet': {'chat': [{"sentence": row['text'],
+                                                                                   "type": type_sent}]}})
         else:
             domanda = row['to']
             if pd.isnull(domanda):
                 testo = {
-                    "dialogueID": id,
+                    "dialogueID": row_id,
                     "chat": [{
                         "sentence": row['text'],
                         "type": "Domanda"
@@ -35,12 +40,10 @@ def load_df_to_mongo(df, mongo_uri, mongo_db):
                 }
             else:
                 testo = {
-                    "dialogueID": id,
+                    "dialogueID": row_id,
                     "chat": [{
                         "sentence": row['text'],
                         "type": "Risposta"
                     }]
                 }
-            print(testo)
-            db.dialogs.insert_one(testo)
-
+            db.dialogs.insert_one(testo) # insert the document in the collection
